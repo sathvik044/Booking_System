42ac40b02d7d029b646aaf21d3ab096b8718a33b
sathvik044 <sathvikvadapalli123@gmail.com>
2026-04-24 11:21:35 +0530

    added hotel entity

---
+++ HotelBackend/src/main/java/com/example/HotelBackend/entity/Hotel.java
@@ -0,0 +1,28 @@
+package com.example.HotelBackend.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="hotels")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private String address;
+    private double rating;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+}
