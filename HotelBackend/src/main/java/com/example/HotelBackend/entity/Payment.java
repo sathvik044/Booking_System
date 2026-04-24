1950da8fa484d4ffaea85412c470ccd1da38929d
Your Name <you@example.com>
2026-04-24 12:02:00 +0530

    Done

--- HotelBackend/src/main/java/com/example/HotelBackend/entity/Payment.java
+++ HotelBackend/src/main/java/com/example/HotelBackend/entity/Payment.java
@@ -2,7 +2,6 @@ package com.example.HotelBackend.entity;
    
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDateTime;
 
 @Entity
--- HotelBackend/src/main/java/com/example/HotelBackend/entity/Room.java
+++ HotelBackend/src/main/java/com/example/HotelBackend/entity/Room.java
@@ -1,8 +1,24 @@
 package com.example.HotelBackend.entity;
 
 import java.time.LocalDateTime;
+
 import com.example.HotelBackend.enums.RoomType;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
